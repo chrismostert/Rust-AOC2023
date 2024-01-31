760f5c07af973a94fa3eff57b124c6882e96b908
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2024-01-31 17:05:59 +0100

    Day 22 part 2

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,5 +1,8 @@
 use itertools::Itertools;
-use std::collections::{HashMap, HashSet};
+use std::{
+    cell::{Cell, RefCell},
+    collections::{HashSet, VecDeque},
+};
 
 struct Point {
     x: usize,
@@ -17,80 +20,104 @@ impl Point {
     }
 }
 
-struct Brick {
+struct Brick<'a> {
     id: usize,
+    top_z: Cell<usize>,
+    bricks_below: RefCell<Vec<&'a Brick<'a>>>,
+    bricks_above: RefCell<Vec<&'a Brick<'a>>>,
     p1: Point,
     p2: Point,
 }
-impl Brick {
+impl Brick<'_> {
     fn from(line: &str, id: usize) -> Self {
         let (p1, p2) = line.split('~').map(Point::from).collect_tuple().unwrap();
-        Brick { id, p1, p2 }
+        Brick {
+            id,
+            top_z: Cell::new(p2.z),
+            bricks_below: RefCell::new(Vec::new()),
+            bricks_above: RefCell::new(Vec::new()),
+            p1,
+            p2,
+        }
     }
     fn coords(&self) -> impl Iterator<Item = (usize, usize)> {
         (self.p1.x..=self.p2.x).cartesian_product(self.p1.y..=self.p2.y)
     }
 }
 
-type SupportMap = HashMap<usize, HashSet<usize>>;
-fn construct_support_graphs(bricks: &[Brick]) -> (SupportMap, SupportMap) {
-    let mut top_bricks: [[Option<(usize, usize)>; 10]; 10] = [[None; 10]; 10];
-    let mut supported_by = (0..bricks.len())
-        .map(|idx| (idx, HashSet::new()))
-        .collect::<SupportMap>();
-    let mut supports = supported_by.clone();
+fn fall_bricks<'a>(bricks: &'a [Brick<'a>]) {
+    let mut top_bricks: [[Option<&'a Brick>; 10]; 10] = [[None; 10]; 10];
 
     for falling_brick in bricks.iter() {
-        let mut z_to_fall_to = 1;
-        let mut possible_supports: Vec<(usize, usize)> = Vec::new();
+        let bricks_we_fall_on = falling_brick
+            .coords()
+            .filter_map(|(x, y)| top_bricks[y][x])
+            .unique_by(|b| b.id)
+            .max_set_by_key(|&brick| brick.top_z.get());
 
-        // Find highest intersection
-        for (x, y) in falling_brick.coords() {
-            if let Some((possible_support, height)) = top_bricks[y][x] {
-                z_to_fall_to = z_to_fall_to.max(height + 1);
-                possible_supports.push((possible_support, height));
-            }
+        for &brick in bricks_we_fall_on.iter() {
+            falling_brick.bricks_below.borrow_mut().push(brick);
+            brick.bricks_above.borrow_mut().push(falling_brick);
         }
 
-        // Add to support graph
-        for supporting_idx in possible_supports
-            .into_iter()
-            .filter(|&(_, height)| height == z_to_fall_to - 1)
-            .map(|(brick_idx, _)| brick_idx)
-            .unique()
+        falling_brick.top_z.set(
+            bricks_we_fall_on
+                .iter()
+                .map(|b| b.top_z.get())
+                .next()
+                .unwrap_or(0)
+                + 1
+                + (falling_brick.p2.z - falling_brick.p1.z),
+        );
+
+        for (x, y) in falling_brick.coords() {
+            top_bricks[y][x] = Some(falling_brick);
+        }
+    }
+}
+
+fn n_safe_bricks(bricks: &[Brick]) -> usize {
+    bricks
+        .iter()
+        .filter(|brick| {
+            brick.bricks_above.borrow().len() == 0
+                || brick
+                    .bricks_above
+                    .borrow()
+                    .iter()
+                    .all(|above| above.bricks_below.borrow().len() > 1)
+        })
+        .count()
+}
+
+fn n_falling_bricks(brick_to_remove: &Brick) -> usize {
+    let mut to_check: VecDeque<&Brick> = brick_to_remove
+        .bricks_above
+        .borrow()
+        .iter()
+        .copied()
+        .collect();
+    let mut fallen = HashSet::from([brick_to_remove.id]);
+
+    while let Some(brick) = to_check.pop_front() {
+        if brick
+            .bricks_below
+            .borrow()
+            .iter()
+            .filter(|b| !fallen.contains(&b.id))
+            .count()
+            == 0
         {
-            supported_by
-                .entry(falling_brick.id)
-                .or_default()
-                .insert(supporting_idx);
-            supports
-                .entry(supporting_idx)
-                .or_default()
-                .insert(falling_brick.id);
-        }
-
-        // Update top_bricks
-        for (x, y) in falling_brick.coords() {
-            top_bricks[y][x] = Some((
-                falling_brick.id,
-                z_to_fall_to + (falling_brick.p2.z - falling_brick.p1.z),
-            ));
+            fallen.insert(brick.id);
+            brick
+                .bricks_above
+                .borrow()
+                .iter()
+                .for_each(|&b| to_check.push_back(b));
         }
     }
 
-    (supported_by, supports)
-}
-
-fn unsafe_bricks(supported_by: &SupportMap) -> impl Iterator<Item = &usize> {
-    supported_by
-        .values()
-        .filter(|vals| vals.len() == 1)
-        .flatten()
-        .unique()
-}
-
-fn n_safe_bricks(supported_by: &SupportMap, bricks: &[Brick]) -> usize {
-    bricks.len() - unsafe_bricks(supported_by).count()
+    fallen.len() - 1
 }
 
 fn main() {
@@ -101,8 +128,11 @@ fn main() {
         .sorted_by(|a, b| a.p1.z.cmp(&b.p1.z))
         .collect_vec();
 
-    let (supported_by, supports) = construct_support_graphs(&bricks);
-    let p1 = n_safe_bricks(&supported_by, &bricks);
+    fall_bricks(&bricks);
+
+    let p1 = n_safe_bricks(&bricks);
+    let p2 = bricks.iter().map(n_falling_bricks).sum::<usize>();
 
     println!("Part 1: {}", p1);
+    println!("Part 2: {}", p2);
 }
